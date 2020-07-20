d0077aae8321fa132b22eef08b99c7f1f6f89c26
poulem <noah.saintonge@dauphine.eu>
2020-07-20 21:51:58 +0200

    Je me demande où les fichiers vont aller

---
+++ letest.py
@@ -0,0 +1,7 @@
+#!/usr/local/anaconda/bin/python3.7
+#-*-coding:Utf-8 -*
+
+print("le code fonctionne bien")
+print("Rien à signaler")
+print('Si vous voyez ce message c\'est que rien n\'a fonctionné')
+print('ou que la manoeuvre n\'a pas encore été réalisée')
